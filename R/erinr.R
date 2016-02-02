a2596bbf81fdee1fa025608dbf7d22426c61e2c8
Kreiling  Erin G <kreilingeg@mathR.math.appstate.edu>
2016-02-01 20:17:00 -0500

    Add read me

---
+++ R/erinr.R
@@ -0,0 +1,8 @@
+#' @description Miscelaneous functions for testing code in class.
+#' @details Explain all kinds of stuff here.
+#' @docType package
+#' @name erinr-package
+#' @title erinr- functions for shuffling cards, rolling dice, taking the square root of numbers, etc.
+#' @section My Custom Section: Add additional information here.
+#' @keywords package,dice,shuffle,roll
+NULL
